5748c147d5719a0389fd29144d84d3a3eb3693d5
salza323 <sal.zamora480@gmail.com>
2020-10-19 12:06:19 -0700

    setup of context API

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-import Login from './Login';
-import Register from './Register';
+import Login from './Components/Login';
+import Register from './Components/Register';
 
 function App() {
   return (
