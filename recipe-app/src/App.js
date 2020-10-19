93ff8105ae53e01833b4eba69debff232ed88205
unknown <xiongjoh@gmail.com>
2020-10-19 14:59:31 -0500

    move styled component out of function to resolve dynamic generation issue

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -7,6 +7,7 @@ import Register from './components/Register';
 import Header from './components/Header';
 
 function App() {
+
   return (
     <div className='App'>
       <Header/>
--- recipe-app/src/Components/Header.js
+++ recipe-app/src/Components/Header.js
@@ -1,42 +1,44 @@
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Link, Route, Switch, useLocation } from 'react-router-dom'
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 
+const SRHeader = styled.header`
+display: flex;
+flex-flow: row nowrap;
+justify-content: space-between;
+align-items: center;
+width: 85%;
+margin: 0 auto;
+nav {
+
+}
+nav a{
+    padding: 5%;
+    text-decoration: none;
+    color: black;
+    border: 1px solid black;
+}
+`
+
 export default function Header() {
-
-    const Header = styled.header`
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: space-between;
-    align-items: center;
-    width: 85%;
-    margin: 0 auto;
-    nav {
-
-    }
-    nav a{
-        padding: 5%;
-        text-decoration: none;
-        color: black;
-        border: 1px solid black;
-    }
-    `
 
 const location = useLocation()
 
     return (
-        <Header>
+        <SRHeader>
             <h1>Secret Recipe</h1>
             <nav>
                 <Link to='/'>Home</Link>
+                <Link to='/login' >Login</Link>
+                <Link to='/register'>Register</Link>
 
-                {location.pathname === '/login' 
-                ? null 
+                {/* {location.pathname === '/login' 
+                ? <Link disabled={true} to='/login' >Login</Link> 
                 : <Link to='/login' >Login</Link>}
                 {location.pathname === '/register' 
-                ? null 
-                :  <Link to='/register'>Register</Link>}
+                ? <Link disabled={true} to='/register'>Register</Link> 
+                :  <Link to='/register'>Register</Link>} */}
             </nav>
-        </Header>
+        </SRHeader>
     )
 }
