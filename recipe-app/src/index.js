1a3fe235086e5848873b18c16a7c71473eb4ef0d
unknown <xiongjoh@gmail.com>
2020-10-19 15:25:01 -0500

    change quotations

--- recipe-app/src/index.js
+++ recipe-app/src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, withRouter } from 'react-router-dom';
 
 const AppWithRouter = withRouter(App);
@@ -10,8 +10,8 @@ const AppWithRouter = withRouter(App);
 ReactDOM.render(
     <Router>
       <AppWithRouter />
-    </Router>
-  document.getElementById("root")
+    </Router>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
