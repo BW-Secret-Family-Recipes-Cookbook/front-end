37bf3f7473f4258f8522a8cc65edc4c727fc2e07
salza323 <sal.zamora480@gmail.com>
2020-10-19 18:19:15 -0700

    built out basic recipes comp & restructured app.js

--- recipe-app/src/Components/Register.js
+++ recipe-app/src/Components/Register.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+import { RegisterContext } from '../contexts/RegisterContext';
 
 const StyledForm = styled.form`
-display: flex;
-flex-flow: column nowrap;
-`
+  display: flex;
+  flex-flow: column nowrap;
+`;
 
 const initialRegisterValues = {
   email: '',
   username: '',
   password: '',
 };
-
 
 const Register = () => {
   const [credentials, setCredentials] = useState(initialRegisterValues);
@@ -36,44 +37,44 @@ const Register = () => {
 
   return (
     <div className='register-form'>
-          
       <StyledForm onSubmit={onSubmit}>
-     <label>Email:
-        <input
-          type='text'
-          name='email'
-          placeholder='email'
-          value={credentials.email}
-          onChange={changeHandler}
-        />
-     </label>
-          <label>Username:
-        <input
-          type='text'
-          name='username'
-          placeholder='username'
-          value={credentials.username}
-          onChange={changeHandler}
-        />
-              </label>
-          <label>Password:
-        <input
-          type='text'
-          name='password'
-          placeholder='password'
-          value={credentials.password}
-          onChange={changeHandler}
-        />
-              </label>
-          <div className='register btn'>
-            <button>Submit</button>
-          </div>
+        <label>
+          Email:
+          <input
+            type='text'
+            name='email'
+            placeholder='email'
+            value={credentials.email}
+            onChange={changeHandler}
+          />
+        </label>
+        <label>
+          Username:
+          <input
+            type='text'
+            name='username'
+            placeholder='username'
+            value={credentials.username}
+            onChange={changeHandler}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type='text'
+            name='password'
+            placeholder='password'
+            value={credentials.password}
+            onChange={changeHandler}
+          />
+        </label>
+        <div className='register btn'>
+          <button>Submit</button>
+        </div>
       </StyledForm>
     </div>
   );
 };
-
-
 
 export default Register;
 
