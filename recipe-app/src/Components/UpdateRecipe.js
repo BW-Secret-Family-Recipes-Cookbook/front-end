a4027622561f8d27f801783fe4c4924a50b8b080
salza323 <sal.zamora480@gmail.com>
2020-10-21 09:21:28 -0700

    added update recipe form for editing

--- recipe-app/src/Components/UpdateRecipe.js
+++ recipe-app/src/Components/UpdateRecipe.js
@@ -23,4 +23,78 @@ const UpdateRecipe = () => {
       [name]: value,
     });
   };
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/recipes${id}`)
+      .then((res) => {
+        setRecipe(res.data);
+      })
+      .catch((err) => {
+        console.log('Error:', err);
+      });
+  }, [id]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .put(`recipes/${recipe.id}`, recipe)
+      .then((res) => {
+        setRecipe(res.data);
+        push('/recipes/all');
+      })
+      .catch((err) => {
+        console.log('Put Error:', err);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Recipe Editor</h2>
+
+      <input
+        name='recipe'
+        type='text'
+        value={recipe.name}
+        onChange={changeHandler}
+        placeholder='Enter Recipe Name'
+      />
+
+      <input
+        name='source'
+        type='text'
+        value={recipe.source}
+        onChange={changeHandler}
+        placeholder='Enter Recipe Name'
+      />
+
+      <input
+        name='instructions'
+        type='text'
+        value={recipe.instructions}
+        onChange={changeHandler}
+        placeholder='Enter Instructions'
+      />
+
+      <input
+        name='category'
+        type='text'
+        value={recipe.category}
+        onChange={changeHandler}
+        placeholder='Enter Instructions'
+      />
+      {/* ^^^^^should change to selector??? */}
+
+      <input
+        name='ingredient'
+        type='text'
+        value={recipe.ingredients}
+        onChange={changeHandler}
+        placeholder='Enter Ingredient'
+      />
+      <button>Submit Changes</button>
+    </form>
+  );
 };
+
+export default UpdateRecipe;
