8a67ef2ca16ab30b8be9871cc560a657a3938eb6
salza323 <sal.zamora480@gmail.com>
2020-10-22 11:46:20 -0700

    edit & delete function now working

--- recipe-app/src/Components/UpdateRecipe.js
+++ recipe-app/src/Components/UpdateRecipe.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import * as yup from 'yup';
 
 import updateSchema from '../validation/updateRecSchema';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
+import { RecipesContext } from '../contexts/RecipesContext';
 
 const initialRecipe = {
   name: '',
@@ -23,28 +24,30 @@ const initialErrors = {
 };
 
 const UpdateRecipe = (props) => {
-  console.log({ recipe: props.recipe });
+  const { recipes, setRecipes } = useContext(RecipesContext);
+  // console.log({ recipe: props.recipe });
   const [recipe, setRecipe] = useState(props.recipe);
   const [errorMessages, setErrorMessages] = useState(initialErrors);
-  const { id } = useParams();
+  const { recipeid } = useParams();
   const { push } = useHistory();
 
   const changeHandler = (e) => {
     const { name, value } = e.target;
-    yup.reach(updateSchema, name)
-    .validate(value)
-    .then(() => {
-      setErrorMessages({
-        ...errorMessages,
-        [name]:'',
+    yup
+      .reach(updateSchema, name)
+      .validate(value)
+      .then(() => {
+        setErrorMessages({
+          ...errorMessages,
+          [name]: '',
+        });
       })
-    })
-    .catch( err => {
-      setErrorMessages({
-        ...errorMessages,
-        [name]: err.errors[0],
-      })
-    })
+      .catch((err) => {
+        setErrorMessages({
+          ...errorMessages,
+          [name]: err.errors[0],
+        });
+      });
 
     setRecipe({
       ...recipe,
@@ -61,7 +64,7 @@ const UpdateRecipe = (props) => {
   //     .catch((err) => {
   //       console.log('Error:', err);
   //     });
-  // }, [id]);
+  // }, [recipeid]);
 
   useEffect(() => {
     console.log({ recipe });
@@ -70,18 +73,33 @@ const UpdateRecipe = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const newArr = [];
-    recipe.ingredients.replace(/,+$/,"").split(',').forEach((ingr) => {
-      newArr.push(ingr);
-    });
+    recipe.ingredients
+      .replace(/,+$/, '')
+      .split(',')
+      .forEach((ingr) => {
+        newArr.push(ingr);
+      });
     const updatedRecipe = {
       ...recipe,
       ingredients: newArr,
     };
+    console.log(updatedRecipe);
     axiosWithAuth()
       .put(`/recipes/${recipe.recipeid}`, updatedRecipe)
       .then((res) => {
-        setRecipe(res.data);
-        push('/recipes/all');
+        console.log(res.data);
+        setRecipes(
+          recipes.map((recipe) => {
+            if (updatedRecipe.recipeid === recipe.recipeid) {
+              console.log('found it');
+              return updatedRecipe;
+            } else {
+              return recipe;
+            }
+          })
+        );
+        props.editHandler();
+        console.log(recipes);
       })
       .catch((err) => {
         console.log('Put Error:', err);
